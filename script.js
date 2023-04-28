f9e1bcb99dd4a0c70f19b4a3992832cf1b10a171
SAI TEJA H R <98202897+Saitejahr@users.noreply.github.com>
2023-04-28 22:58:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ const byteSize = (str) => {
   // write your code here
 	const utf8Bytes = new TextEncoder().encode(str);
   // Return the length of the array (i.e., the size of the string in bytes)
-  return str.length;
+  return utf8Bytes.length;
 };
 
 // Do not change the code below
+
 const str = prompt("Enter some string.");
 alert(byteSize(str));
